d801a61dcdddfc8e33fc750e30cd0f3a6701997a
Justin <justin.thareja@gmail.com>
2023-01-17 12:56:01 -0800

    use nextjs

---
+++ src/pages/api/sms/reply.js
@@ -0,0 +1,31 @@
+const { MessagingResponse } = require("twilio").twiml;
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const twiml = new MessagingResponse();
+    const message = twiml.message();
+    const body = req.body.Body.toLowerCase().trim();
+
+    if (body === "map") {
+      message.body(
+        "Thanks for your interest! Here's a map to the yoga studio."
+      );
+      message.media("/images/yoga_map.jpg");
+    } else if (body === "schedule") {
+      message.body(
+        "Thanks for your interest! Here's a link to the schedule: https://www.plumasyoga.com/"
+      );
+    } else {
+      message.body(
+        `Thanks for messaging the plumas yoga cooperative, this is an automated service. \n\nIf you're looking for a map to the yoga studio reply with MAP. \n\nIf you'd like to know more about upcoming classes reply with SCHEDULE.`
+      );
+    }
+
+    res
+      .setHeader("Content-Type", "text/xml")
+      .status(200)
+      .send(twiml.toString());
+  } else {
+    res.status(404).send();
+  }
+}
